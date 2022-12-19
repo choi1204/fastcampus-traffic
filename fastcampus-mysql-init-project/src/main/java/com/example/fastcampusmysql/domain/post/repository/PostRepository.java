56d0616d53148a025bc7a664fc0b1c3dd2e82e63
choi hyun woong <choihyeon0517@naver.com>
2022-12-20 00:00:03 +0900

    feat: 내가 쓴글 캘린더 구현

---
+++ fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/domain/post/repository/PostRepository.java
@@ -0,0 +1,100 @@
+package com.example.fastcampusmysql.domain.post.repository;
+
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
+import com.example.fastcampusmysql.domain.post.entity.Post;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class PostRepository {
+    static final String TABLE = "Post";
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private static final RowMapper<DailyPostCount> DAILY_POST_COUNT_MAPPER = (ResultSet resultSet, int rowNum) -> new DailyPostCount(
+            resultSet.getLong("memberId"),
+            resultSet.getObject("createdDate", LocalDate.class),
+            resultSet.getLong("count")
+    );
+
+    public Post save(Post post) {
+        if (post.getId() == null)
+            return insert(post);
+        return update(post);
+    }
+
+    public List<DailyPostCount> groupByCreatedDate(DailyPostCountRequest request) {
+        var sql = String.format("""
+                SELECT createdDate, memberId, count(id)
+                FROM %s
+                WHERE memberId = :memberId AND createdDate between :firstDate and :lastDate
+                GROUP BY createdDate, memberId
+                """, TABLE);
+        var params = new BeanPropertySqlParameterSource(request);
+        return namedParameterJdbcTemplate.query(sql, params, DAILY_POST_COUNT_MAPPER);
+    }
+
+    private Post insert(Post post) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(namedParameterJdbcTemplate.getJdbcTemplate())
+                .withTableName(TABLE)
+                .usingGeneratedKeyColumns("id");
+
+        SqlParameterSource params = new BeanPropertySqlParameterSource(post);
+        var id = jdbcInsert.executeAndReturnKey(params).longValue();
+
+        return Post.builder()
+                .id(id)
+                .memberId(post.getMemberId())
+                .contents(post.getContents())
+                .createdDate(post.getCreatedDate())
+                .createdAt(post.getCreatedAt())
+                .build();
+    }
+
+
+    private Post update(Post post) {
+        var sql = String.format("""
+                UPDATE %s set 
+                    memberId = :memberId, 
+                    contents = :contents, 
+                    createdDate = :createdDate, 
+                    createdAt = :createdAt, 
+                    likeCount = :likeCount,
+                    version = :version + 1 
+                WHERE id = :id and version = :version
+                """, TABLE);
+
+        SqlParameterSource params = new BeanPropertySqlParameterSource(post);
+        var updatedCount = namedParameterJdbcTemplate.update(sql, params);
+        if (updatedCount == 0) {
+            throw new RuntimeException("not updated");
+        }
+        return post;
+    }
+
+    public void bulkInsert(List<Post> posts) {
+        var sql = String.format("""
+                INSERT INTO `%s` (memberId, contents, createdDate, createdAt)
+                VALUES (:memberId, :contents, :createdDate, :createdAt)
+                """, TABLE);
+
+        SqlParameterSource[] params = posts
+                .stream()
+                .map(BeanPropertySqlParameterSource::new)
+                .toArray(SqlParameterSource[]::new);
+        namedParameterJdbcTemplate.batchUpdate(sql, params);
+    }
+}
+
